313664edd247198f88ebc284aa4b62485d5811d7
Nabhag Motivaras <nabhagmotivaras76@gmail.com>
2022-10-23 17:36:35 +0530

    feat: enabled preflight cors

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ app.use(cors({
     preflightContinue: false,
 }));
 
+app.options('*', cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }));
 
@@ -34,7 +35,7 @@ app.use(verifyJwt.unless({
     path: ["/token", new RegExp("^/auth"), "/"]
 }));
 // app.use(compression());
-appolling.create('/auth/:code', ( req, res, next) => {
+appolling.create('/auth/:code', cors(), ( req, res, next) => {
     req.id =req.params.code;
     next();
 });
