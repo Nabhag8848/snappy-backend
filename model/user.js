999c565d80dfcce6964383b09fb442034c6f1368
Nabhag Motivaras <nabhagmotivaras76@gmail.com>
2022-12-19 11:48:47 +0530

    FIX: payment integration URL

--- model/user.js
+++ model/user.js
@@ -126,7 +126,7 @@ userSchema.statics.isUserExist = async function({
 
         if(!user){
 
-            const customer = await fetch('/payment/create-customer', {
+            const customer = await fetch( 'https://snappy.up.railway.app/payment/create-customer', {
                 method: 'post',
                 headers: {
                   'Content-Type': 'application/json',
